1fc4bcad44a571dfbaf9faf51f84c74336253e2d
Sakuzy _Peng <86222913+SakuzyPeng@users.noreply.github.com>
2026-08-31 23:51:30 +0800

    fix(gui): reserve more space for bitstream info

--- src/app.rs
+++ src/app.rs
@@ -678,7 +678,7 @@ impl PlayerApp {
                     .inner_margin(egui::Margin::same(18)),
             )
             .show(root, |ui| {
-                const INFO_BLOCK_HEIGHT: f32 = 205.0;
+                const INFO_BLOCK_HEIGHT: f32 = 220.0;
                 const BLOCK_GAP: f32 = 18.0;
 
                 let available = ui.available_rect_before_wrap();
